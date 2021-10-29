0e15ffb2aa42145a2fa4d389a152e00a2f438bb9
Wyatt726 <wyatt726@aol.com>
2021-10-29 15:33:08 -0400

    re org of functions, adding in random optins function

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function random_item(items)
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
+
+// the arrays of trip options
+let cities = ["NYC", "Baltimore" , "NOLA", "Raleigh"];
+let restraunt = ["pizza", "crab cakes" , "gumbo", "bbq"];
+let vic = ["walkin", "train" , "vespa", "razor scooter"];
+let entertainment = [];
+
+// function that picks random element from array
+function generateRandomSelection(array){
+    let randomValue = Math.floor(Math.random() * array.length)
+    randomValue = array[randomValue]
+    return randomValue
 }
 
-let items = ["NYC", "Baltimore" , "NOLA", "Raleigh"];
-console.log(random_item(items));
+// variables that hold the randomly selected element from array
+let randomCities = generateRandomSelection(cities)
+console.log(randomCities)
+
+
+// array to hold all 4 selections
+let randomOptions = [randomCities, randomRestraunt]
+
+
+// is there a way to pass randomOptions into a re select funcion. 
