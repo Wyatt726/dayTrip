e3e2d10c1e43045b83514e39b3a488d9bf4aa088
Wyatt726 <wyatt726@aol.com>
2021-11-01 11:45:18 -0400

    completed the boolean function for the reselect trip option. project fully functional

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let cities = ["NYC", "Baltimore" , "NOLA", "Raleigh"];
 let restraunt = ["pizza", "crab cakes" , "gumbo", "bbq"];
 let vic = ["walkin", "train" , "vespa", "razor scooter"];
-let entertainment = [];
+let ent= ["stand-Up Comedy", "Movies", "Downtown Bars", "Sight Seeing"];
 
 // function that picks random element from array
 function generateRandomSelection(array){
@@ -15,10 +15,29 @@ function generateRandomSelection(array){
 // variables that hold the randomly selected element from array
 let randomCities = generateRandomSelection(cities)
 console.log(randomCities)
+let randomRestraunt = generateRandomSelection(restraunt)
+console.log(randomRestraunt)
+let randomVic = generateRandomSelection (vic)
+console.log(randomVic)
+let randomEntertainment = generateRandomSelection(ent)
+console.log(randomEntertainment)
 
 
 // array to hold all 4 selections
-let randomOptions = [randomCities, randomRestraunt]
+let randomOptions = [randomCities, randomRestraunt, randomVic, randomEntertainment]
 
 
 // is there a way to pass randomOptions into a re select funcion. 
+
+const input = prompt("do you like your trip? enter yes or no")
+let no = false
+if (no){
+    console.log (generateRandomSelection)   
+}
+    else{
+        alert("Have a fun trip!")
+    }
+    
+
+
+
